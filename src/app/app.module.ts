11b622f2e73ce5f3e4cf5adffe8732ca7e6ad2b0
Anuj Sharma <anuj.sharma01@nagarro.com>
2023-03-22 18:17:03 +0530

    sadasd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { SpotifyLoginModule } from './spotify-login/spotify-login.module';
-import { APP_BASE_HREF } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -16,9 +15,7 @@ import { APP_BASE_HREF } from '@angular/common';
     FormsModule,
     SpotifyLoginModule
   ],
-  providers: [
-    { provide: APP_BASE_HREF, useValue: (window as any)['_app_base'] || '/' },
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
